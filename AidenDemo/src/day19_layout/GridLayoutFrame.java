f98889abf63a7977a36133a31ddd1cff1fee0684
ParaHuang <paradox041@gmail.com>
2025-09-13 10:08:18 +0800

    grid layout

--- AidenDemo/src/day19_layout/GridLayoutFrame.java
+++ AidenDemo/src/day19_layout/GridLayoutFrame.java
@@ -33,8 +33,8 @@ public class GridLayoutFrame extends JFrame {
 		});
 	}
 
-	private int rows=10;
-	private JButton[] btns = new JButton[rows*rows];
+	private int amount=10;
+	private JButton[] btns = new JButton[amount*amount];
 	
 	public GridLayoutFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,7 +44,7 @@ public class GridLayoutFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(rows, rows,2,2));
+		contentPane.setLayout(new GridLayout(amount, amount,2,2));
 		
 		try {
 		    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
@@ -61,60 +61,53 @@ public class GridLayoutFrame extends JFrame {
 			contentPane.add(btns[i]);
 //			int index = i;
 			
-			btns[i].addActionListener(new ActionListener() {
+			btns[i].addActionListener(e->{
+				//1.inside anonymous inner class,we can't use local variable,unless it's final
+				//2.in a higher java version,if you never change the value of a variable
+				//you can use this local variable inside anonymous inner class,
+				//because the jvm would take it as final
+				//3.inside anonymous inner class, you can use global variable all as you what
 				
-				//event records every detail the it happens
-				@Override
-				public void actionPerformed(ActionEvent e) {
-//					System.out.println(x);
-					//1.inside anonymous inner class,we can't use local variable,unless it's final
-					//2.in a higher java version,if you never change the value of a variable
-					//you can use this local variable inside anonymous inner class,
-					//because the jvm would take it as final
-					//3.inside anonymous inner class, you can use global variable all as you what
-					
-					
-					
-					//which button we clicked
-					//solution 1:define a temporary variable to save i, then use it here
-//					System.out.println(index);
-					//solution 2:mark every button outside,get the mark here
-					String mark = e.getActionCommand();
-//					System.out.println(command);
-					int index = Integer.parseInt(mark);
-					System.out.println(index);
-					//current button
-					if(btns[index].getBackground() == Color.PINK) {
-						btns[index].setBackground(Color.BLUE);
-					}else {
-						btns[index].setBackground(Color.PINK);
-					}
-					
-					//left
-					if(btns[index-1].getBackground() == Color.PINK) {
-						btns[index-1].setBackground(Color.BLUE);
-					}else {
-						btns[index-1].setBackground(Color.PINK);
-					}
-					//right
-					if(btns[index+1].getBackground() == Color.PINK) {
-						btns[index+1].setBackground(Color.BLUE);
-					}else {
-						btns[index+1].setBackground(Color.PINK);
-					}
-					//up
-					if(btns[index-rows].getBackground() == Color.PINK) {
-						btns[index-rows].setBackground(Color.BLUE);
-					}else {
-						btns[index-rows].setBackground(Color.PINK);
-					}
-					//down
-					if(btns[index+rows].getBackground() == Color.PINK) {
-						btns[index+rows].setBackground(Color.BLUE);
-					}else {
-						btns[index+rows].setBackground(Color.PINK);
-					}
-					
+				
+				
+				//which button we clicked
+				//solution 1:define a temporary variable to save i, then use it here
+//				System.out.println(index);
+				//solution 2:mark every button outside,get the mark here
+				String mark = e.getActionCommand();
+//				System.out.println(command);
+				int index = Integer.parseInt(mark);
+				System.out.println(index);
+				//current button
+				if(btns[index].getBackground() == Color.PINK) {
+					btns[index].setBackground(Color.BLUE);
+				}else {
+					btns[index].setBackground(Color.PINK);
+				}
+				
+				//left
+				if(btns[index-1].getBackground() == Color.PINK) {
+					btns[index-1].setBackground(Color.BLUE);
+				}else {
+					btns[index-1].setBackground(Color.PINK);
+				}
+				//right
+				if(btns[index+1].getBackground() == Color.PINK) {
+					btns[index+1].setBackground(Color.BLUE);
+				}else {
+					btns[index+1].setBackground(Color.PINK);
+				}
+				//up
+				if(btns[index-amount].getBackground() == Color.PINK) {
+					btns[index-amount].setBackground(Color.BLUE);
+				}else {
+					btns[index-amount].setBackground(Color.PINK);
+				}
+				//down
+				if(btns[index+amount].getBackground() == Color.PINK) {
+					btns[index+amount].setBackground(Color.BLUE);
+				}else {
+					btns[index+amount].setBackground(Color.PINK);
 				}
 			});
 		}
